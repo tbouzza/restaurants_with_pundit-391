744125a36d51fcf22ebe73e206274549ffcaf0af
Tarig Bouazzati <tbouzza@gmail.com>
2020-05-19 10:14:52 +0200

    pundit implemented

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,29 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    is_owner_or_admin?
+  end
+
+  def destroy?
+    is_owner_or_admin?
+  end
+
+  private
+
+  def is_owner_or_admin?
+    @record.user == @user || @user.admin
+  end
+end
